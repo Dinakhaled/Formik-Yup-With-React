e5a900e5d937daac21fcb63888fafd2adb7648ab
Dinakhaled <dina.elghndour@gmail.com>
2020-07-18 13:53:17 +0200

    Error handled

--- src/Containers/Signup/Signup.js
+++ src/Containers/Signup/Signup.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Input, Switch, Space } from 'antd';
-import { Formik, Form } from 'formik';
+import { Input, Switch, Space, Button } from 'antd';
+import { Formik, Form, ErrorMessage, Field } from 'formik';
 import * as yup from 'yup';
 
 class Signup extends Component {
@@ -8,23 +8,60 @@ class Signup extends Component {
     super(props);
     this.state = {  }
   }
+
+  SignupSchema = yup.object().shape({
+    fullName: yup.string().required('This field is required.'), 
+    email: yup.string().email().required('This field is required.'), 
+    password: yup.string()
+        .min(6, 'Password is too short.')
+        .max(10, 'Password is too long.')
+        .required('This field is required.')
+  });
+
   render() { 
     return ( 
-      <form style={{width: '400px'}}>
+      <div style={{width: '400px'}}>
         <h1>Sign Up</h1>
-        <Space direction="vertical" style={{width: '100%'}}>
-          <label>Full Name</label>
-          <Input />
-          <label>Email</label>
-          <Input />
-          <label>Password</label>
-          <Input />
-          <label>Confirm Password</label>
-          <Input />
-          <label>Are you student?</label>
-          <Switch defaultChecked/>
-        </Space>
-      </form>
+      <Formik
+          initialValues={{
+          fullName: '', 
+          email: '',  
+          password: ''
+        }}
+        validationSchema={this.SignupSchema}
+        onSubmit={values => {
+          console.log(values);
+        }}
+      >
+      {({errors, handleChange, handleBlur, touched}) => (
+          <Form>
+            <Space direction="vertical" style={{width: '100%'}}>
+              <label>Full Name</label>
+              {/* <Input onChange={handleChange} onBlur={handleBlur} name='fullName' /> */}
+              <Field name="fullName" component={Input} />
+              <ErrorMessage name="fullName">
+                {errMsg => <span className="errorMessage">{errMsg}</span>}
+              </ErrorMessage>
+              <label>Email</label>
+              <Input onChange={handleChange} onBlur={handleBlur} name='email' />
+              <ErrorMessage name="email">
+                {errMsg => <span className="errorMessage">{errMsg}</span>}
+              </ErrorMessage>
+              <label>Password</label>
+              <Input onChange={handleChange} onBlur={handleBlur} name='password' />
+              <ErrorMessage name="password">
+                {errMsg => <span className="errorMessage">{errMsg}</span>}
+              </ErrorMessage>
+              <label>Confirm Password</label>
+              <Input onChange={handleChange} />
+              <label>Are you student?</label>
+              <Switch onChange={handleChange} defaultChecked/>
+              <Button htmlType='submit' style={{textAlign: 'center', width: '100%'}} type='primary'>Sign up</Button>
+            </Space>
+          </Form>
+      )}
+      </Formik>
+      </div>
     );
   }
 }
